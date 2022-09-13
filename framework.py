a05d0ae5f46211f66c90d7de148ac972b55c925c
Yiyuan Han <y.han@essex.ac.uk>
2022-09-13 18:58:49 +0100

    Debug to make the training epochs run
    Debug finished

--- framework.py
+++ framework.py
@@ -21,6 +21,7 @@ num_epochs = 5 # Number of training epochs
 lr = 0.0001 # Learning rate for Optimizers
 beta1 = 0.5 # Beta1 hyperparam for Adam optimizer
 ngpu = 0 # Number of GPUs (CHANGE IT WHEN RUNNING ON THE HPC)
+
 class AdversarialModel:
     def __init__(self, data_samples, class_label, subject_label):
          ### Parameters ###
@@ -35,8 +36,21 @@ class AdversarialModel:
         self.data_samples = data_samples
         self.class_label = class_label
         self.subject_label = subject_label
+
+        # Create the training and testing datasets
+        # Shuffle the indices
+        dataset_indices = list(range(self.data_samples.shape[0]))
+
+        random.shuffle(dataset_indices)
+        train_idx = dataset_indices[0:int(0.75 * self.data_samples.shape[0])]
+        test_idx = dataset_indices[int(0.75 * self.data_samples.shape[0]):]
+
+
         # Create the dataloader
-        self.painDataset = trans_net.PainDataset(data_samples, class_label, subject_label)
+        self.painDataset_train = trans_net.PainDataset(data_samples[train_idx], class_label[train_idx], subject_label[train_idx])
+        self.painDataset_test = trans_net.PainDataset(data_samples[test_idx], class_label[test_idx], subject_label[test_idx])
+
+
 
         # Get the number of classes and subjects involved in the classeification
         class_unique = np.unique(class_label)
@@ -58,54 +72,57 @@ class AdversarialModel:
         self.main_optim = torch.optim.Adam(self.main_clf.parameters(), lr=0.001, betas=(beta1, 0.999))
         self.adv_optim = torch.optim.Adam(self.adv_clf.parameters(), lr=0.001, betas=(beta1, 0.999))
 
-    def train(self, log_path): 
-        # Define the device
-        device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+    def train(self): 
         # Training Loop
 
         # Lists to keep track of progress
-        img_list = []
         main_losses = []
         adv_losses = []
         iters = 0   
 
         # For each epoch
         for epoch in range(num_epochs):
+            
             # For each batch in the dataloader
-            for i, data in enumerate(self.painDataset, 0):
+            dataloader = DataLoader(self.painDataset_train, batch_size=batch_size, shuffle=True)
+            for i, data in enumerate(dataloader, 0):
+                
                 ################
                 # Update the main classifier (minimize the loss)
                 self.main_clf.zero_grad()
                 # Format the batch
-                data_sample = data[0]
-                label = data[1]
+                data_sample = data['data_sample']
+                label = data['class']
                 # Forward the labels through the main classifier
                 output = self.main_clf(data_sample)
                 # Calculate the loss
                 main_loss = self.criterion(output, label)
                 # Calculate the gradients for the main_clf in backward pass
-                main_loss.backward()
+                
                 main_x = output.mean().item()
-                self.main_optim.step()
+                
 
             
 
                 ########################
                 # Train the adversary classifier (maximize the loss)
                 self.adv_clf.zero_grad()
-                label = data[2].to(device)
+                label = data['subject'].long()
                 output = self.adv_clf(data_sample)
                 # Loss of the adversary model
                 adv_loss = self.criterion(output, label)
+
+                # Optimize the losses
                 mix_loss = main_loss - adv_loss # For maximizing the main loss and minimize the adversary loss
                 mix_loss.backward()
                 self.adv_optim.step()
+                self.main_optim.step()
                 adv_x = output.mean().item()
 
                  # Output training stats
                 if i % 50 == 0:
-                    print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                        % (epoch, num_epochs, i, len(self.painDataset),
+                    print('[%d/%d][%d/%d]\tLoss_main: %.4f\tLoss_adv: %.4f\tLoss(x): %.4f\tD(G(x)): %.4f / %.4f'
+                        % (epoch, num_epochs, i, len(self.painDataset_train),
                             main_loss.item(), adv_loss.item(), mix_loss.item(), main_x, adv_x))
 
                 # Save Losses for plotting later
@@ -113,6 +130,8 @@ class AdversarialModel:
                 adv_losses.append(adv_loss.item())
 
                 iters += 1
+
+        return main_losses, adv_losses
 
 
 
--- trans_net.py
+++ trans_net.py
@@ -1,28 +1,34 @@
 import torch
 import torch.nn as nn
-import torch.nn.function as F
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+import numpy as np
 
 # The main classifier for classifying the pain-related conditions
 class main_clf(nn.Module):
     def __init__(self, class_count):
         super().__init__()
-        self.conv1 = nn.Conv2D(1, 128, 7, padding_mode='same')
+        self.conv1 = nn.Conv2d(1, 128, 7, padding=3, stride=1)
         self.pool = nn.MaxPool2d(3)
-        self.norm = nn.BatchNorm2d()
-        self.conv2 = nn.Conv2D(128, 64, 5, padding_mode='same')
-        self.conv3 = nn.Conv2D(64, 32, 3, padding_mode='same')
+        self.norm1 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(128, 64, 5, padding=2, stride=1)
+        self.norm2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 32, 3, padding=1, stride=1)
+        self.norm3 = nn.BatchNorm2d(32)
         self.dropout = nn.Dropout2d(p=0.2)
-        self.fc1 = nn.Linear(100)
-        self.fc2 = nn.Linear(class_count)
+        self.fc1 = nn.Linear(in_features=32, out_features=100)
+        self.fc2 = nn.Linear(in_features=100, out_features=class_count)
         self.softmax = nn.Softmax()
 
 
     
     def forward(self, x):
-        x = self.norm(self.pool(F.relu(self.conv1(x))))
-        x = self.norm(self.pool(F.relu(self.conv2(x))))
-        x = self.norm(self.pool(F.relu(self.conv3(x))))
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        x = self.norm1(x)
+        x = self.norm2(self.pool(F.relu(self.conv2(x))))
+        x = self.norm3(self.pool(F.relu(self.conv3(x))))
         x = self.dropout(x)
         # Flatten
         x = torch.flatten(x, 1)
@@ -36,9 +42,9 @@ class main_clf(nn.Module):
 class adv_clf(nn.Module):
     def __init__(self, sub_count):
         super().__init__()
-        self.conv = nn.Conv2D(1, 16, 3, padding_mode='same')
-        self.norm = nn.BatchNorm2d()
-        self.fc = nn.Linear(sub_count)
+        self.conv = nn.Conv2d(1, 16, 3, padding=1, stride=1)
+        self.norm = nn.BatchNorm2d(16)
+        self.fc = nn.Linear(in_features=16384, out_features=sub_count)
         
     def forward(self, x):
         x = self.norm(self.conv(x))
@@ -50,18 +56,27 @@ class adv_clf(nn.Module):
 # The class for building the dataset with both class labels and subject labels for tuning
 class PainDataset(Dataset):
     def __init__(self, data_samples, class_labels, subject_labels):
+
         self.data_samples = data_samples
         self.class_labels = class_labels
-        self.subject_labels = subject_labels
+        # Convert the subject labels into sequential int labels for classification
+        subject_unique, unique_idx = np.unique(subject_labels, return_inverse=True)
+        subject_unique = subject_unique.tolist()
+        # subject_count = subject_unique.shape[0]
+        subject_labels_n = []
+        for subject_label in subject_labels:
+            subject_labels_n.append(subject_unique.index(subject_label))
+        self.subject_labels = subject_labels_n
     
     def __len__(self):
-        return self.data_sample.shape[0]
+        return self.data_samples.shape[0]
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         
-        data_sample = torch.from_numpy(self.data_samples[idx])
+        orig_shape = self.data_samples[idx].shape
+        data_sample = torch.from_numpy(np.reshape(self.data_samples[idx], newshape=(orig_shape[2], orig_shape[0], orig_shape[1]))).float()
         class_label = self.class_labels[idx]
         subject_label = self.subject_labels[idx]
 
@@ -71,4 +86,8 @@ class PainDataset(Dataset):
 
 # The function for initializing the weights of two networks
 def weights_init(m):
-    nn.init.normal_(m.weight.data, 0.0, 0.02)
+    if isinstance(m, nn.Conv2d):
+        m.weight.data.normal_(0.0, 0.02)
+    elif isinstance(m, nn.BatchNorm2d):
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
